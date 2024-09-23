a74595a4cd33646b2e35e2798a97118765b108a7
M Starch <LeStarch@googlemail.com>
2024-09-22 17:41:00 -0700

    Moving replay to pipeline level

--- gst/replay.py
+++ gst/replay.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 
 from .pipeline import FILESRC_ELEMENT_NAME, FILEQUEUE_ELEMENT_NAME
 import gi
@@ -7,13 +8,17 @@ from gi.repository import Gst
 class ReplayFile(object):
     def __init__(self, pipeline):
         """ Replay a file on EOS """
+        self.pipeline = pipeline
         self.filesrc = pipeline.get_by_name(FILESRC_ELEMENT_NAME)
-        self.filequeue = pipeline.get_by_name(FILEQUEUE_ELEMENT_NAME)
 
     async def force_replay(self):
         """ Force a replay every 30 seconds """
         while True:
-            await asyncio.sleep(5)
+            await asyncio.sleep(random.randrange(0, 10))
             if self.filesrc is None:
                 return
-#            self.filesrc.seek_simple(Gst.Format.TIME, 0, 0)
+            self.pipeline.set_state(Gst.State.PAUSED)
+            self.pipeline.seek(1.0, Gst.Format.TIME, 
+                (Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT),
+                Gst.SeekType.SET, 0.0 , Gst.SeekType.NONE, -1)
+            self.pipeline.set_state(Gst.State.PLAYING)
